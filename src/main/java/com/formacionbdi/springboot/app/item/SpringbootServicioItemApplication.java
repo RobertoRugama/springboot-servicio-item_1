f131092baa923cdd18e57fcffb5b63335b740d37
Roberto Rugama <roberto.rugama01@gmail.com>
2022-02-22 23:15:58 -0600

    Se configuró la aplicacion para ser leida desde eureka-server
    1- Se modificó el pom.xml agregando nuevas dependencias para que pueda ser leido por eureka-server, se comenta la dependencia spring-cloud-starter-netflix-ribbon ya que se esta asiendo uso de eurka.
    
    2- Se comenta la improtacion de ribbonClient y se agrega la anotacion EnableEurekaClient para que la aplicacion sea leida por el servidor de eureka como cliente.

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,11 +2,13 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "servicio-productos")
-@EnableFeignClients
+@EnableEurekaClient
+//@RibbonClient(name = "servicio-productos") Se comenta servicio ya que se esta usando eurekaClient
+@EnableFeignClients  // se deja feignClients para poder conectarnos a las apiRest y se inyectado con Autowired
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
