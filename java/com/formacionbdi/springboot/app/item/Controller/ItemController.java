c24d912ac5459ddebed2fc5432050b561ea8ba40
Roberto Rugama <roberto.rugama01@gmail.com>
2022-02-28 09:45:41 -0600

    Tolerancia de Fallos y Latencia con Hystrix
    1- se configura el Pom.xml Agregando la dependencia netflix-hystrix
    2- se habilita con la anotacion Enablecircuitbreaker en la clase principal de la aplicacion o servicio.
    3- Se trabajo en la clase ItemController una especie de metodo alternativo para simular la repuesta exitosa en caso que detecte fallos.

--- java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
+++ java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -24,9 +26,24 @@ public class ItemController {
 		return itemService.FindAll();
 	}
 	
+	@HystrixCommand(fallbackMethod="metodoAlternativo")
 	@GetMapping("/show/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		
 		return itemService.findById(id, cantidad);
 	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);		
+		return item;
+		
+	}
+	
+	
 }
