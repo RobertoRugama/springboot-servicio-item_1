3c9417a806eb99412e7d11d8258980a731d3421a
Roberto Rugama <roberto.rugama01@gmail.com>
2022-03-15 11:19:57 -0600

    46- Personalizando Parametros del circuit Breaker
    Comentamos en el application.properties el optional:configServer solo para la prueba de la configuracion de parametros para circuitBreaker.
    2- Se agregó la clase Customizer con la anotacion @Bean usando Resiliencie4jCircuitBreaker

--- java/com/formacionbdi/springboot/app/item/AppConfig.java
+++ java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -1,9 +1,17 @@
 package com.formacionbdi.springboot.app.item;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 @Configuration
 public class AppConfig {
 
@@ -11,5 +19,21 @@ public class AppConfig {
 	@LoadBalanced  /*Va_utilizar_Ribben_para_balanceo_de_Carga_haciendo_uso_de_Restemplate*/
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id -> {
+			return new Resilience4JConfigBuilder(id)
+					.circuitBreakerConfig(CircuitBreakerConfig.custom()
+					.slidingWindowSize(10)
+					.failureRateThreshold(50)
+					.waitDurationInOpenState(Duration.ofSeconds(10L))
+					.permittedNumberOfCallsInHalfOpenState(5)
+					.build())
+			.timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+			.build();
+			
+		});
 	}
 }
