dd23bc87ab9d4dd2051e470334272374d338058f
Roberto Rugama <roberto.rugama01@gmail.com>
2022-03-14 23:02:46 -0600

    43- Configurando microservicio items con Resilience4J
    1- se configuro primero el archivo pom.xml cambiando la version de la 2.3.12.RELEASE a la version 2.5.3
    2- se cambio la version en las propiedades spring-cloud.version>2020.0.3<spring.cloud.version>
    3- se comentó la dependencia hystrix.
    4-  Se configuró el application.properties agregadon el cofig.import=optional:configserver
    5-En la clase principal se comenta la anotacion @EnableCircuitBreaker
    6- se crea la propiedad CircuitBreakerFactorty, se comenta la anotacion de Hystringcommand  y se modifica el metodo item detalle

--- java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
+++ java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,11 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.formacionbdi.springboot.app.item.models.Item;
 import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
 
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
 	@Autowired
 	//@Qualifier("serviceFeign")  //Balanceo_de_carga_usando_ribbon_y_feign
 	@Qualifier("serviceRestTemplate")  // Balanceo_de_Carga_usando_ribbon_y_RestTemplate
@@ -30,11 +35,11 @@ public class ItemController {
 		return itemService.FindAll();
 	}
 	
-	@HystrixCommand(fallbackMethod="metodoAlternativo")
+	//@HystrixCommand(fallbackMethod="metodoAlternativo") // se comenta porque se trabajara con Resilience4j
 	@GetMapping("/show/{id}/cantidad/{cantidad}")
-	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		
-		return itemService.findById(id, cantidad);
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {	
+		return cbFactory.create("items")
+				.run(()-> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad));
 	}
 	
 	public Item metodoAlternativo(Long id, Integer cantidad) {
--- java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,12 +2,12 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableCircuitBreaker
+//@EnableCircuitBreaker // Se comenta porque se va trabajar con Resilience4j
 @EnableEurekaClient
 //@RibbonClient(name = "servicio-productos") Se comenta servicio ya que se esta usando eurekaClient
 @EnableFeignClients  // se deja feignClients para poder conectarnos a las apiRest y se inyectado con Autowired
