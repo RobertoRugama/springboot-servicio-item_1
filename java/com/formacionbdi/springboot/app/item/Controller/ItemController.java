bcf04bafa8f562949d4a6f78b829c4ff630f0529
Roberto Rugama <roberto.rugama01@gmail.com>
2022-03-18 17:24:06 -0600

    52- Anotacion @CircuitBreaker
    1- se Creo un nuevo metodo mas llamado items show3 don de se usa la clase CompletableFuture y retornamos a trabes de su metdo suppyAsync
    2- Se crea otro nuvo metodo lamado Metodo alternativo2 con el fin de ser invocado una vez generada la excepcion del timeLimiter
    3- se hace una conbinacion entre el CircuitBreaker y TiemLimiter y se valida que se pueden combinar ambas funcionalidades
    4- se cambio en el application.proerties el tiempo de duracion de timeout de 6s a 2 s

--- java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
+++ java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.item.Controller;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,7 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 //import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -53,6 +55,13 @@ public class ItemController {
 		return itemService.findById(id, cantidad);
 	}
 	
+	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name="items", fallbackMethod = "metodoAlternativo2")
+	@GetMapping("/show3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {	
+		return CompletableFuture.supplyAsync(()-> itemService.findById(id, cantidad));
+	}
+	
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
 		logger.info(e.getMessage());
 		Item item = new Item();
@@ -63,6 +72,19 @@ public class ItemController {
 		producto.setPrecio(500.00);
 		item.setProducto(producto);		
 		return item;
+		
+	}
+	
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		Item item = new Item();
+		Producto producto = new Producto();
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);		
+		return CompletableFuture.supplyAsync(()-> item);
 		
 	}
 	
