fc4f80f93005fdaf5b25432e16be42b235810426
RobertoRugama <roberto.rugama01@gmail.com>
2022-03-23 00:06:57 -0600

    70-71-72 Implementando Handler CRUD Consumiendo desde API Rest con clienteFeign
    1- se agregaron los metodos crear, update y eliminar  en la clase ProductoClienteRest.
    2- En la clase IitemController se agregan los metodos crud para ServicesRestTemplatey SevicesFeign

--- java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
+++ java/com/formacionbdi/springboot/app/item/Controller/ItemController.java
@@ -15,10 +15,15 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
@@ -39,12 +44,12 @@ public class ItemController {
 	@Autowired
 	private Environment env;
 	
-	@SuppressWarnings("rawtypes")
+    @SuppressWarnings("rawtypes")
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
 	@Autowired
-	//@Qualifier("serviceFeign")  //Balanceo_de_carga_usando_ribbon_y_feign
-	@Qualifier("serviceRestTemplate")  // Balanceo_de_Carga_usando_ribbon_y_RestTemplate
+	@Qualifier("serviceFeign")  //Balanceo_de_carga_usando_ribbon_y_feign
+	//@Qualifier("serviceRestTemplate")  // Balanceo_de_Carga_usando_ribbon_y_RestTemplate
 	private ItemService itemService;
 	
 	@Value("${configuracion.texto}")
@@ -114,6 +119,25 @@ public class ItemController {
 		}
 		
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+	
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return itemService.save(producto);
+	}
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		return itemService.update(producto, id);
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		itemService.delete(id);
+	
 	}
 	
 	
